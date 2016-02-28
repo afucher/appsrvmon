824745f156f3dae13723d75a4d961b34138b6dc2
Arthur Fücher <arthurfucher@gmail.com>
2016-02-28 20:48:36 -0300

    initial commit

---
+++ bin/appsrvmon.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+var program = require('commander');
+var spawn = require('../lib/spawn.js');
+var path = require('path');
+
+program
+  .version('0.0.1')
+  .option('-a, --appserver <path/to/appserver>', 'path to appserver.exe');
+
+program.parse(process.argv);
+
+let path_appserver = program.appserver || '';
+path_appserver = path.resolve(path_appserver,'.\\appserver.exe');
+
+let command = {
+  path : path_appserver
+};
+//command.push(path_appserver);
+console.log(command);
+spawn(command);
