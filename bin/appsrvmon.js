fb2c4df3541d156b1c2119d6782e7cfefd6707b4
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-05-14 11:56:39 -0300

    Criada opção para abrir o INI e tratamento para o nome do server conforme a plataforma.

--- bin/appsrvmon.js
+++ bin/appsrvmon.js
@@ -6,14 +6,16 @@ var path = require('path');
 var fs = require('fs');
 var pkg = JSON.parse(fs.readFileSync(__dirname + '/../package.json'));
 
+let serverName = process.platform == 'win32' ? 'appserver.exe' : 'appsrvlinux';
+
 program
   .version(pkg.version)
-  .option('-a, --appserver <path/to/appserver>', 'path to appserver.exe');
+  .option('-a, --appserver <path/to/appserver>', 'path to ' + serverName );
 
 program.parse(process.argv);
 
 let path_appserver = program.appserver || '';
-path_appserver = path.resolve(path_appserver,'.\\appserver.exe');
+path_appserver = path.resolve(path_appserver,'.\\' + serverName );
 
 let command = {
   path : path_appserver
