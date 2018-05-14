662bff7aa21ff374e8a5ac4e97a756464ed30ffa
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-05-14 15:11:40 -0300

    Criada opção que permite não monitorar o appserver.ini, pois algumas vezes a rotina altera o mesmo, exemplo: Schedule.

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ let serverName = process.platform == 'win32' ? 'appserver.exe' : 'appsrvlinux';
 program
   .version('0.0.2')
   .option('-a, --appserver <path/to/appserver>', 'path to ' + serverName )
-  .option('-c, --smartclient <path/to/smartclient>', 'path to smartclient' );
+  .option('-c, --smartclient <path/to/smartclient>', 'path to smartclient' )
+  .option('-w, --watchini <watch/ini/file>', 'watch ini file?' );
 
 program.parse(process.argv);
 
 let path_client = program.smartclient;
+let watchINI = program.watchini;
 let path_appserver = program.appserver || '';
+
 path_appserver = path.resolve(path_appserver,'.\\' + serverName );
 
 let command = {
   path : path_appserver
 };
-//command.push(path_appserver);
-console.log(command);
-spawn(command,path_client);
+
+spawn(command,path_client,watchINI);
