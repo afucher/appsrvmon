cec77077e7674aef15eb7748be87d73b96a84748
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-05-14 11:56:39 -0300

    Criada opção para abrir o INI e tratamento para o nome do server conforme a plataforma.

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ var program = require('commander');
 var spawn = require('./lib/spawn.js');
 var path = require('path');
 
+let serverName = process.platform == 'win32' ? 'appserver.exe' : 'appsrvlinux'
+
 program
   .version('0.0.1')
-  .option('-a, --appserver <path/to/appserver>', 'path to appserver.exe');
+  .option('-a, --appserver <path/to/appserver>', 'path to ' + serverName );
 
 program.parse(process.argv);
 
 let path_appserver = program.appserver || '';
-path_appserver = path.resolve(path_appserver,'.\\appserver.exe');
+path_appserver = path.resolve(path_appserver,'.\\' + serverName );
 
 let command = {
   path : path_appserver
